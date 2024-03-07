17b63bfdebdc69b0c9e1844539d14c80de862e55
Murtaza Dhanerawala <116756472+zamurda@users.noreply.github.com>
2024-03-07 22:55:06 +0000

    changed response generation code
    Add update_tmpl option to response_generator() which allows llm to take in context from previous messages.
    
    add Arize Phoenix as llm query tracer

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 import streamlit as st
-import random
-import time
+import phoenix as px
+import logging
+from llama_index.core import PromptTemplate, set_global_handler
 
 from query_pipeline import pipeline
+from prompts import summary_tmpl_with_mem_str
 
-def response_generator(prompt: str) -> str:
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+px.launch_app()
+set_global_handler('arize_phoenix')
+
+def response_generator(prompt: str, update_tmpl: PromptTemplate=None) -> str:
+    if update_tmpl:
+        pipeline.dict()['module_dict']['synthesizer']['synthesizer'].update_prompts(
+            {'summary_template': update_tmpl}
+        )
     response = pipeline.run(topic=prompt)
     return response
 
 def add_to_message_history(role: str, content: str) -> None:
     message = {"role": role, "content": str(content)}
     st.session_state.messages.append(message)  # Add response to message history
+
+def generate_memory_str_from_messages(session_state: st.session_state) -> str:
+    question = session_state.messages[-1]['content']
+    answer = session_state.messages[-2]['content']
+    return f'User\'s previous question: {question}\nYour previous answer: {answer}'
 
 
 st.set_page_config(
@@ -49,6 +65,14 @@ if prompt := st.chat_input(
         if st.session_state.messages[-1]["role"] != "assistant":
             with st.chat_message("assistant"):
                 with st.spinner("Thinking..."):
-                    response = (str(response_generator(prompt)))
+                    # update prompt with the last assistant message for context
+                    if len(st.session_state.messages) >= 3: # can only update with context if there is atleast 1 Q and 1 A in the message history
+                        prompt_with_mem_tmpl = PromptTemplate(summary_tmpl_with_mem_str.format(
+                            memory_str=generate_memory_str_from_messages(st.session_state),
+                            )
+                        )
+                        response = (str(response_generator(prompt, update_tmpl=prompt_with_mem_tmpl)))
+                    else:
+                        response = (str(response_generator(prompt)))
                     st.write(response)
                     add_to_message_history("assistant", (response))
